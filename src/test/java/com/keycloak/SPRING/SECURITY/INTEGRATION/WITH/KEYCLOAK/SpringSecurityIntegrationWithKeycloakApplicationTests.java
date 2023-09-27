48548e72b997ffaced54bc6bedd0802746776856
Saurabh Maithani <saurabhmaithani01921@gmail.com>
2023-09-27 18:35:06 +0530

    initial commit

---
+++ src/test/java/com/keycloak/SPRING/SECURITY/INTEGRATION/WITH/KEYCLOAK/SpringSecurityIntegrationWithKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keycloak.SPRING.SECURITY.INTEGRATION.WITH.KEYCLOAK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityIntegrationWithKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
